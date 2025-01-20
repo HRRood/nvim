c7e6a11ab0c6ab5d6aabd3b2a1e2f893af4cda30
Roy Roodenburg <roy.roodenburg@pangaea.nl>
2025-01-20 09:46:52 +0100

    Harpoon keybinds changes

--- init.lua
+++ init.lua
@@ -805,7 +805,7 @@ require("lazy").setup({
 			harpoon:setup()
 			-- REQUIRED
 
-			vim.keymap.set("n", "<leader>a", function()
+			vim.keymap.set("n", "<C-a>", function()
 				harpoon:list():add()
 			end)
 			vim.keymap.set("n", "<C-e>", function()
@@ -815,13 +815,13 @@ require("lazy").setup({
 			vim.keymap.set("n", "<A-h>", function()
 				harpoon:list():select(1)
 			end)
-			vim.keymap.set("n", "<A-t>", function()
+			vim.keymap.set("n", "<A-j>", function()
 				harpoon:list():select(2)
 			end)
-			vim.keymap.set("n", "<A-n>", function()
+			vim.keymap.set("n", "<A-k>", function()
 				harpoon:list():select(3)
 			end)
-			vim.keymap.set("n", "<A-s>", function()
+			vim.keymap.set("n", "<A-l>", function()
 				harpoon:list():select(4)
 			end)
 
