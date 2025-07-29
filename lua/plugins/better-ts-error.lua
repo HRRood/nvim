4eac296f38f3d2cf2581845abad5fa2c82a5ea9a
Roy Roodenburg <hrroodenburg@gmail.com>
2025-07-29 23:57:42 +0200

    better ts error

--- lua/plugins/better-ts-error.lua
+++ lua/plugins/better-ts-error.lua
@@ -1,13 +1,20 @@
 return {
-	"OlegGulevskyy/better-ts-errors.nvim",
-	dependencies = { "MunifTanjim/nui.nvim" },
-	config = function()
-		require("better-ts-errors").setup({
-			keymaps = {
-				toggle = "<leader>dd", -- Toggle the error explanation
-				go_to_definition = "<leader>dx", -- Jump to the error's definition
-			},
-		})
-	end,
-	event = "LspAttach", -- Ensure the plugin loads when the LSP attaches
+	{
+		"OlegGulevskyy/better-ts-errors.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("better-ts-errors").setup({
+				keymaps = {
+					toggle = "<leader>dd", -- Toggle the error explanation
+					go_to_definition = "<leader>dx", -- Jump to the error's definition
+				},
+			})
+		end,
+		event = "LspAttach", -- Ensure the plugin loads when the LSP attaches
+	},
+	{
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+	},
 }
